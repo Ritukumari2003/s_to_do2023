36ddbba1b487fe6072f30f50993f5d5837af547e
Ritu <2503.ritu@gmail.com>
2023-11-26 13:36:04 +0530

    finished project

---
+++ app/src/main/java/com/example/todolistinkotlin/ToDoListData.kt
@@ -0,0 +1,10 @@
+package com.example.todolistinkotlin
+
+
+class ToDoListData(
+    val title: String = "",
+    val date: String = "",
+    val time: String = "",
+    var indexDb: Long = 0,
+    val isShow : Int = 0
+)
